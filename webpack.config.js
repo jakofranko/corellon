7411db5edaacb0df0dbdb08f4dc01f38ff142bf1
Jake Franklin <jacob.d.franklin@gmail.com>
2019-02-28 22:34:54 -0700

    Set up webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const webpack = require("webpack");
+
 module.exports = {
     mode: 'development',
     module: {
@@ -7,10 +10,26 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader",
                 options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
     resolve: {
         extensions: ['*', '.js', '.jsx']
-    }
+    },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 }
