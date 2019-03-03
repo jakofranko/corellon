29000b184af3646119e1f565763e6180d9b51dc7
Jake Franklin <jacob.d.franklin@gmail.com>
2019-03-02 22:45:01 -0700

    Work on adding entries to journal

--- src/journal.js
+++ src/journal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react' ;
 import { phi, tableFor } from './phi';
+import Entry from './entry';
 
 class Journal extends Component {
     constructor(props) {
@@ -8,14 +9,34 @@ class Journal extends Component {
         this.state = {
             entries: []
         };
+
+        this.addEntry = this.addEntry.bind(this);
+        this.addEvent = this.addEvent.bind(this);
     }
 
-    addEntry(events) {
+    addEntry(e) {
+        const journal = e.target.closest(".journal");
+        let events = [];
+        document.querySelectorAll('.new-event').forEach((el) => events.push(el.value));
+
         this.setState((currentState, props) => {
+            const newEntries = currentState.entries.concat([{ events }])
             return {
-                entries: currentState.entries.push({ events })
+                entries: newEntries
             };
-        })
+        });
+
+        journal.querySelectorAll(".new-event").forEach((el) => el.remove());
+    }
+
+    addEvent(e) {
+        e.preventDefault();
+        let event = document.createElement("input");
+        event.classList = "new-event";
+        event.placeholder = "Event";
+
+        e.target.closest(".new-events").prepend(event);
+        event.focus();
     }
 
     getUniqueEvents() {
@@ -32,7 +53,19 @@ class Journal extends Component {
     }
 
     render() {
-        return <div className="journal">I am a Journal</div>;
+        const entries = this.state.entries.map((entry, index) => <Entry key={index} events={entry.events} />);
+        return (
+            <div className="journal">
+                <h1>Journal:</h1>
+                <label>Add Event</label>
+                <form className="new-events">
+                    <input className="new-event" placeholder="Event" />
+                    <button className="add-event" onClick={this.addEvent}>+</button>
+                </form>
+                <button className="add-entry" onClick={this.addEntry}>Add Entry</button>
+                {entries}
+            </div>
+        );
     }
 }
 
