b4f03df756149602b50d92503d4496f5c2f6f665
Jake Franklin <jacob.d.franklin@gmail.com>
2019-03-18 21:07:50 -0700

    Added new journal functionality

--- src/js/journal-list.js
+++ src/js/journal-list.js
@@ -3,17 +3,46 @@ import React, { Component } from 'react';
 import Journal from './journal';
 
 class JournalList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            addingJournal: false
+        };
+
+        this.addJournal = this.addJournal.bind(this);
+    }
+
+    addJournal(e) {
+        e.preventDefault();
+        this.setState({
+            addingJournal: true
+        });
+    }
+
+    componentDidUpdate() {
+        // If we just added a journal, switch this off
+        if (this.state.addingJournal) {
+            this.setState({
+                addingJournal: false
+            });
+        }
+    }
+
     render() {
         const journalIds = Object.keys(localStorage).filter(key => key.match("journal-"));
         const journals = journalIds.map(id => <Journal key={id} journalId={id} />);
 
         return (
             <div className="journal-list">
+                <h1 className="bb lhs pb2">Journals</h1>
                 {
                     journals.length
                     ? journals
                     : <Journal />
                 }
+                {this.state.addingJournal && <Journal />}
+                <button className="bg-blanc ba db wf f2 fwb pv2" onClick={this.addJournal}>New Journal +</button>
             </div>
         )
     }
