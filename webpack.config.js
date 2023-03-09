c60276af390274c71bfbe3448e04bb56436d3f7d
Jake Franklin <jacob.d.franklin@gmail.com>
2023-03-09 08:58:25 -0700

    Add workbox and update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    GenerateSW
+} = require('workbox-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = function(env, args) {
     return {
-        mode: args.mode || 'development',
+        mode,
         module: {
             rules: [
                 {
@@ -27,16 +34,53 @@ module.exports = function(env, args) {
             filename: "bundle.js"
         },
         devServer: {
-            contentBase: path.join(__dirname, "public/"),
             port: 3000,
-            publicPath: "http://localhost:3000/dist/",
-            hotOnly: true
+            compress: true,
+            static: {
+                directory: path.join(__dirname, 'public'),
+            },
         },
         plugins: [
             new webpack.HotModuleReplacementPlugin(),
             new webpack.DefinePlugin({
                 'build.mode': JSON.stringify(args.mode)
-            })
+            }),
+            new HtmlWebpackPlugin({
+                title: 'Corellon 🌙',
+                favicon: './public/assets/favicon.png',
+                template: './src/index.ejs',
+                meta: {
+                    'mobile-web-app-capable': 'yes',
+                    'apple-mobile-web-app-capable': 'yes',
+                    'application-name': 'Corellon',
+                    'apple-mobile-web-app-title': 'Corellon',
+                    'theme-color': '#444444',
+                    'msapplication-navbutton-color': '#444444',
+                    'apple-mobile-web-app-status-bar-style': 'black-translucent',
+                    'msapplication-starturl': '/',
+                    'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no'
+                }
+            }),
+            new CopyPlugin({
+                patterns: [{
+                        from: './public/assets',
+                        to: 'assets'
+                    },
+                    {
+                        from: './public/manifest.json',
+                        to: 'manifest.json'
+                    }
+                ]
+            }),
+            new GenerateSW({
+                mode,
+                clientsClaim: true,
+                skipWaiting: true,
+                runtimeCaching: [{
+                    urlPattern: /\.js$/,
+                    handler: 'NetworkFirst'
+                }]
+            }),
         ]
     };
 }
